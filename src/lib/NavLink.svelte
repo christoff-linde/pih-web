18790f939a14fee77f008ced7f146f4f399a2bef
christoff-linde <christoff.linde@gmail.com>
2025-01-08 20:34:22 +0200

    feat: add NavLink component & use in layout for navbar

---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		to: string;
+	}
+	let { title, to }: Props = $props();
+</script>
+
+<a class="rounded-md bg-gray-200 p-2 transition-all hover:bg-gray-300 hover:font-semibold" href={to}
+	>{title}</a
+>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import NavLink from '$lib/NavLink.svelte';
 	import '../app.css';
 	let { children } = $props();
+	console.log($page);
 </script>
 
-{@render children()}
+<div class="mx-2 font-mono">
+	<nav class="my-2 flex gap-2">
+		<NavLink to="/" title="home" />
+		<NavLink to="/twin" title="twin" />
+		<NavLink to="/sensors" title="sensors" />
+		<NavLink to="/readings" title="readings" />
+	</nav>
+	<p class="mb-2">Currently at {$page.url.pathname}</p>
+	{@render children()}
+</div>
